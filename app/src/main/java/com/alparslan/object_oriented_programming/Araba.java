f2cd10c022515f4518b15368dc5ec452d6df8e73
Alparslan Ozden <ahmetalparslanozden@gmail.com>
2022-03-21 23:53:56 +0300

    New Kotlin Project

---
+++ app/src/main/java/com/alparslan/object_oriented_programming/Araba.java
@@ -0,0 +1,10 @@
+package com.alparslan.object_oriented_programming;
+
+public class Araba {
+
+    var renk:String = "Kırmızı"
+    var hiz:Int = 150
+    var calisiyorMu:Boolean = false
+
+
+}
---
+++ app/src/main/java/com/alparslan/object_oriented_programming/ArabaMain.kt
@@ -0,0 +1,14 @@
+package com.alparslan.object_oriented_programming
+
+fun main(){
+
+    val bmw = Araba()
+
+    println(bmw.renk)
+    println(bmw.hiz)
+    println(bmw.calisiyorMu)
+
+    bmw.renk = "Beyaz"
+
+
+}

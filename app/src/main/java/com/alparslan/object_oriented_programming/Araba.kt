e2f03af29c0df454ba5229e3ff3a8485fc04425f
Alparslan Ozden <ahmetalparslanozden@gmail.com>
2022-03-23 03:21:26 +0300

    Code Refactor

--- app/src/main/java/com/alparslan/object_oriented_programming/Araba.kt
+++ app/src/main/java/com/alparslan/object_oriented_programming/Araba.kt
@@ -1,9 +1,30 @@
 package com.alparslan.object_oriented_programming
 
-class Araba {
-    var renk:String =  "Kırmızı"
-    var hiz:Int =  150
-    var calisiyorMu:Boolean = false
+class Araba (var renk:String,var hiz:Int,var calisiyorMu:Boolean) {
+
+
+    fun calistir(){
+        calisiyorMu = true
+    }
+
+    fun durdur(){
+        calisiyorMu = false
+    }
+
+    fun hizlan(kacKm:Int){
+    hiz = hiz + kacKm
+    }
+
+    fun yavasla(kacKm: Int){
+        hiz = hiz - kacKm
+    }
+
+    fun bilgiAl(){
+        println("--------------------------")
+        println("Renk: $renk")
+        println("Hız: $hiz")
+        println("Çalışıyor mu: $calisiyorMu")
+    }
 
 
 
--- app/src/main/java/com/alparslan/object_oriented_programming/ArabaMain.kt
+++ app/src/main/java/com/alparslan/object_oriented_programming/ArabaMain.kt
@@ -2,13 +2,30 @@ package com.alparslan.object_oriented_programming
 
 fun main(){
 
-    val bmw = Araba()
+    val bmw = Araba(renk = "Kırmızı", hiz = 100, calisiyorMu = true)
+    val sahin = Araba(renk = "Beyaz", hiz = 90, calisiyorMu = false)
 
+    bmw.bilgiAl()
+    bmw.durdur()
+    bmw.hizlan(100)
+    bmw.bilgiAl()
+    bmw.yavasla(20)
+    bmw.bilgiAl()
+    sahin.bilgiAl()
+
+    /*
     println(bmw.renk)
     println(bmw.hiz)
     println(bmw.calisiyorMu)
 
     bmw.renk = "Beyaz"
+    bmw.hiz = 200
+    bmw.calisiyorMu = true
 
+    println("--------------------")
+    println(bmw.renk)
+    println(bmw.hiz)
+    println(bmw.calisiyorMu)
+    */
 
 }

1a799776220d1045b79d97dbfe5df1e2aaa7d3c1
Alparslan Ozden <ahmetalparslanozden@gmail.com>
2022-03-23 03:45:45 +0300

    Code Refactor

---
+++ app/src/main/java/com/alparslan/object_oriented_programming/NullSafety.kt
@@ -0,0 +1,10 @@
+package com.alparslan.object_oriented_programming
+
+fun main(){
+
+    var str1:String? = null      // NULL OLABİLİR
+    str1?.trim()                //NULL DEĞİLSE KULLAN
+    // str1!!.trim()              // NULL OLSA BİLE KULLAN
+    var otobus:Otobus? = null
+
+}
--- app/src/main/java/com/alparslan/object_oriented_programming/OtobusMain.kt
+++ app/src/main/java/com/alparslan/object_oriented_programming/OtobusMain.kt
@@ -4,7 +4,6 @@ fun main(){
 
     val kamilKoc = Otobus(kapasite = 50, nereden = "Bursa","Ankara", mevcutYolcu = 10 )
     val metro = Otobus(kapasite = 60, nereden = "Ankara","İstanbul", mevcutYolcu = 40 )
-
     kamilKoc.yazdır()
     metro.yazdır()
 

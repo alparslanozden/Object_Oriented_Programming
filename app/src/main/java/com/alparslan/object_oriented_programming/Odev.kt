90f087d52df89022b28681caedca07d9aa1cbb63
Alparslan Ozden <ahmetalparslanozden@gmail.com>
2022-03-30 01:20:18 +0300

    Code Refactor - Homework

--- app/src/main/java/com/alparslan/object_oriented_programming/Odev.kt
+++ app/src/main/java/com/alparslan/object_oriented_programming/Odev.kt
@@ -4,12 +4,35 @@ import java.util.*
 
 class Odev {
 
-fun derece() {
-    var scanner = Scanner(System.`in`)
+    fun derece() {
+        var scanner = Scanner(System.`in`)
     println("Celcius değerini giriniz: ")
-    var c = scanner.nextInt()
-    var f = (c * 1.8) + 32
+        var c = scanner.nextInt()
+        var f = (c * 1.8) + 32
     println("Fahrenheit değeri: $f")
-
 }
+
+    fun cevreHesabi(){
+        var scanner = Scanner(System.`in`)
+    println("Dikdörtgenin uzun kenarını giriniz: ")
+        var uzun = scanner.nextInt()
+    println("Dikdörtgenin kısa kenarını giriniz: ")
+        var kisa = scanner.nextInt()
+        var cevre = kisa*uzun
+        println("Dikdörtgenin çevresi: $cevre m2")
+    }
+
+
+    fun faktoriyel(){
+        var scanner = Scanner(System.`in`)
+    println("Faktöriyeli hesaplanacak sayıyı giriniz: ")
+        var sayi = scanner.nextInt()
+        for (i in 1..sayi){
+            var sonuc= i*1
+            i+1
+            return
+        }
+    }
+
+
 }
--- app/src/main/java/com/alparslan/object_oriented_programming/OdevMain.kt
+++ app/src/main/java/com/alparslan/object_oriented_programming/OdevMain.kt
@@ -2,7 +2,18 @@ package com.alparslan.object_oriented_programming
 
 fun main(){
 
+   /*
+
     var derece = Odev() //Celcius değerini Fahrenheite çeviren fonksiyon yazıp kullandık
     derece.derece()
 
+
+
+    var dikdortgen = Odev() //Dikdörtgenin çevresini hesaplayan fonksiyon
+    dikdortgen.cevreHesabi()
+
+    */
+
+    var faktoriyel = Odev()
+    faktoriyel.faktoriyel()
 }
